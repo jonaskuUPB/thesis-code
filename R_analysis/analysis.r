2e523f5c0a11bab458e8455fac20c45d429b0d1e
Jonas Kuckling <jonasku@mail.upb.de>
2017-06-09 10:11:59 +0200

    Added Analysis file in R

---
+++ R_analysis/analysis.r
@@ -0,0 +1,31 @@
+init <- function() {
+  setwd("/home/jonas/Documents/Master/thesis-code/results")
+}
+
+loadGeneration <- function(expName = "2017_06_01-10_47_18", expId = 0, genNumber = 0, genSize = 50, agglo = mean){
+  file <- paste(expName, expId, sep = "/")
+  file <- paste(file, "actions", sep = "/")
+  file <- paste(file, "action_gen", sep ="/")
+  file <- paste(file, genNumber, sep = "_")
+  t <- read.table(file)
+  result <- vector(,genSize)
+  for (i in 1:genSize-1){
+    result[i] <- agglo(as.numeric(t[i,]))
+  }
+  # print(result)
+  return(result)
+}
+
+ks_test <- function(){
+  init()
+  vec <- vector(,100)
+  for(genCount in 1:99){
+    print(genCount)
+    evolution <- loadGeneration(expId = 0, agglo = median, genNumber = genCount)
+    random <- loadGeneration(expId = 1, agglo = median, genNumber = genCount)
+    ks_result = ks.test(evolution, random, exact = TRUE)
+    # print(ks_result$p.value)
+    vec[genCount] <- ks_result$p.value
+  }
+  plot(vec)
+}
