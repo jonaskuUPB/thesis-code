e3c408e1f76aa5d28755550e02a6b253dc3f2129
Jonas Kuckling <jonasku@mail.upb.de>
2017-06-14 09:27:09 +0200

    Added kvm_test for the Kramer von Mises Test
    It doesn't seem to work and causes R to crash

--- R_analysis/analysis.r
+++ R_analysis/analysis.r
@@ -16,16 +16,35 @@ loadGeneration <- function(expName = "2017_06_01-10_47_18", expId = 0, genNumber
   return(result)
 }
 
-ks_test <- function(){
+loadGenerationBehaviourEstimate <- function(expName = "2017_06_01-10_47_18", expId = 0, genNumber = 0, genSize = 50, agglo = mean){
+  
+}
+
+ks_test <- function(expName = "2017_06_01-10_47_18"){
   init()
   vec <- vector(,100)
   for(genCount in 1:99){
     print(genCount)
-    evolution <- loadGeneration(expId = 0, agglo = median, genNumber = genCount)
-    random <- loadGeneration(expId = 1, agglo = median, genNumber = genCount)
+    evolution <- loadGeneration(expName = expName, expId = 0, agglo = median, genNumber = genCount)
+    random <- loadGeneration(expName = expName, expId = 1, agglo = median, genNumber = genCount)
     ks_result = ks.test(evolution, random, exact = TRUE)
     # print(ks_result$p.value)
     vec[genCount] <- ks_result$p.value
+  }
+  plot(vec)
+}
+
+kvm_test <- function(expName = "2017_06_01-10_47_18") {
+  init()
+  vec <- vector(,100)
+  for(genCount in 1:99){
+    print(genCount)
+    evolution <- loadGeneration(expName = expName, expId = 0, agglo = median, genNumber = genCount)
+    random <- loadGeneration(expName = expName, expId = 1, agglo = median, genNumber = genCount)
+    cvm <- CvM2SL2Test::cvmts.test(evolution, random)
+    p <- CvM2SL2Test::cvmts.pval(cvm, 50, 50) 
+    # print(ks_result$p.value)
+    vec[genCount] <- p
   }
   plot(vec)
 }
