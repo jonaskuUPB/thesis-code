6251c91ce4f9f2576b00c039c06ed3b93b802963
Jonas Kuckling <jonasku@mail.upb.de>
2017-07-18 18:42:36 +0200

    Updated NSGA2 library and included test run in main file

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 #include <sys/stat.h>
 #include <random>
 #include <sys/types.h>
+#include "nsga2.h"
 
 bool gui = false;
 std::map<std::string, std::string> default_settings;
@@ -32,6 +33,27 @@ int guiRun(int argc, char** argv){
 
 int main(int argc, char** argv)
 {
+
+    NSGA2Type nsga2Params;
+    void *inp = NULL;
+    void *out = NULL;
+
+    double *minReal = (double *)malloc(2*sizeof(double));
+    minReal[0] = 0;
+    minReal[1] = 0;
+    double *maxReal = (double *)malloc(2*sizeof(double));
+    maxReal[0] = 5;
+    maxReal[1] = 3;
+
+    int *numBits = (int *)malloc(1*sizeof(int));
+    double *minBin = (double *)malloc(1*sizeof(double));
+    double *maxBin = (double *)malloc(1*sizeof(double));
+
+    nsga2Params = SetParameters(0.5, 100, 150, 2, 2, 2, minReal, maxReal, 0.9, 0.5, 10, 20, 0, numBits, minBin, maxBin, 0, 0, 1, 1, 2, 0, 0, 0, 0);
+    InitNSGA2(&nsga2Params, inp, out);
+    NSGA2(&nsga2Params, inp, out);
+    exit(0);
+
     // read default settings from file
     default_settings = Utils::readSettingsFrom("results/default_settings");
     srand(time(NULL));
