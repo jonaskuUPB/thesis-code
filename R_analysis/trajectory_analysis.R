c3aabfb77baf35291289fb57777589f54c00a149
Jonas Kuckling <jonasku@mail.upb.de>
2017-09-10 21:07:02 +0200

    Now displays trajectories for all files

--- R_analysis/trajectory_analysis.R
+++ R_analysis/trajectory_analysis.R
@@ -8,3 +8,14 @@ analyse_trajectory <- function(filename) {
   transparent_grey <- rgb(125,125,125, max=255, alpha=16)
   plot(t, col=transparent_grey, pch=1, cex=0.1, xlab="", ylab="", xaxs="i", yaxs="i", xlim=c(0,1280), ylim=c(720,0))
 }
+
+analyse_trajectories <- function(expName) {
+  filename_base <- paste(expName, "run_0", "trajectory", "trajectories_generation_0_genome", sep="/")
+  for(i in 0:51){
+    filename <- paste(filename_base,i, sep="_")
+    print(filename)
+    if(file.exists(filename)) {
+      analyse_trajectory(filename)
+    }
+  }
+}
